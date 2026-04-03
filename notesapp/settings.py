2ff81853a11c6a8aefee0b13f037def15836bdb3
MR.KSHEERA SAGAR SHIVANSH <shivanshks2006@gmail.com>
2026-04-03 21:22:48 +0530

    Switch database from SQLite to MySQL

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'notesapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test_db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',   # 🔥 container name
+        'PORT': '3306',
     }
 }
 
